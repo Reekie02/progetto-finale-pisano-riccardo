77124d465d30ae7f2593cc6f45ac24de72e15d15
Riccardo Pisano <riccardopisano02@gmail.com>
2025-11-06 20:19:03 +0100

    icon modificata con import

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,4 +1,4 @@
-// src/components/CardItem.jsx
+import icon from '../assets/favicon-02.png'
 export default function CardItem({ title, description, imageUrl }) {
     return (
         // <article className="bg-white rounded-lg shadow hover:shadow-md transition p-4">
@@ -10,7 +10,7 @@ export default function CardItem({ title, description, imageUrl }) {
                     loading="lazy"
                     className="w-full h-full object-cover"
                 />
-                <img src="../../public/favicon-02.png" className="absolute right-3 top-3 w-15 h-15" alt="" />
+                <img src={icon} className="absolute right-3 top-3 w-15 h-15" alt="" />
             </div>
             <div className=" p-6 py-10">
                 <h3 className="text-2xl font-bold mb-1 line-clamp-1">{title}</h3>
--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,6 +1,23 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+// src/supabase/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+    // messaggio chiaro in produzione
+    throw new Error("Missing env VITE_SUPABASE_URL");
+}
+if (!supabaseAnonKey) {
+    throw new Error("Missing env VITE_SUPABASE_ANON_KEY");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
